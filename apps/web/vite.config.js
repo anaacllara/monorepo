a5eb71978e76f83c594cfd45965e8ca835a30b91
Sabrina-Abre <sabrina.cardoso@sempreceub.com>
2025-11-14 17:05:54 -0300

    Décimo commit: Modificando arquivo vite.config.js

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Coloque o nome do seu repositório aqui!
-  // Ex: se o repo é 'github.com/romess/bootcamp-pwa', coloque '/bootcamp-pwa/'
-  base: '/monorepo/', 
+  base: '/', 
 })
